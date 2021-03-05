4b365757c31e4d0c089773f43a319a33f71f84d4
vittaya25 <vittayalp@gmail.com>
2021-03-05 14:29:42 +0700

    v1.1.0

---
+++ index.js
@@ -0,0 +1,8 @@
+/**   mainmenu  **/
+function openNav() {
+  document.getElementById("mySidenav").style.width = "30%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
